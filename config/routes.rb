733dc515b84188e66255603979568b976a039dc6
Pat Conley <pconley312@gmail.com>
2016-08-18 06:41:14 -0700

    basic login with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  
+  root to: "home#index"
+  
+  get 'home/index'
+  get 'people/index', to: 'people#index', as: :user_root
+
+  devise_for :users
+  
   resources :people
   resources :widgets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+   devise_scope :user do 
+     get "/some/route" => "some_devise_controller" 
+     get 'login', controller: 'devise/sessions', action: 'create'
+     get 'logout', controller: 'devise/sessions', action: 'destroy'
+     
+   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817022721) do
+ActiveRecord::Schema.define(version: 20160818011101) do
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
@@ -31,6 +31,23 @@ ActiveRecord::Schema.define(version: 20160817022721) do
     t.string   "fams_key"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "widgets", force: :cascade do |t|
