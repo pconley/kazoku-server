8ede8e1cdbc282e1f1dcc391ec8c74ea3fc6c8ba
Pat Conley <pconley312@gmail.com>
2016-09-18 07:52:52 -0700

    cors5

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,33 +3,45 @@ class MembersController < ApplicationController
 	protect_from_forgery
 
 	before_filter :cors_preflight_check
+
 	after_filter :cors_set_access_control_headers
 
-# For all responses in this controller, return the CORS access control headers.
+	before_action :authenticate #, except: [ :index ]
 
-def cors_set_access_control_headers
-  headers['Access-Control-Allow-Origin'] = '*'
-  # headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  # headers['Access-Control-Allow-Headers'] = '*'
-  headers['Access-Control-Max-Age'] = "1728000"
+	def cors_set_access_control_headers
+	  headers['Access-Control-Allow-Origin'] = '*'
+	  # headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+	  # headers['Access-Control-Allow-Headers'] = '*'
+	  headers['Access-Control-Max-Age'] = "1728000"
+	  headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+	  headers['Access-Control-Allow-Headers'] = '*,authorization,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+	end
 
-   headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-   headers['Access-Control-Allow-Headers'] = '*,authorization,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-end
+	def cors_preflight_check
+		# If this is a preflight OPTIONS request, then short-circuit the
+		# request, return only the necessary headers and return an empty
+		# text/plain.
+		if request.method == :options
+		    headers['Access-Control-Allow-Origin'] = '*'
+		    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+		    headers['Access-Control-Allow-Headers'] = '*'
+		    headers['Access-Control-Max-Age'] = '1728000'
+		    render :text => '', :content_type => 'text/plain'
+		end
+	end
 
-# If this is a preflight OPTIONS request, then short-circuit the
-# request, return only the necessary headers and return an empty
-# text/plain.
+	TOKEN = "secret"
 
-def cors_preflight_check
-  if request.method == :options
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = '1728000'
-    render :text => '', :content_type => 'text/plain'
-  end
-end
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        # Compare the tokens in a time-constant manner, to mitigate
+        # timing attacks.
+        ActiveSupport::SecurityUtils.secure_compare(
+          ::Digest::SHA256.hexdigest(token),
+          ::Digest::SHA256.hexdigest(TOKEN)
+        )
+      end
+    end
   
   # GET /members.json
   def index
