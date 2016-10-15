90de01ea06c4a3cc279f0508ce9ebaf3858aff40
Pat Conley <pconley312@gmail.com>
2016-10-15 12:59:02 -0700

    remove people model

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,15 @@ Rails.application.routes.draw do
   
   resources :members
   resources :families
+
   root to: "home#index"
+  get 'home/index', to: 'home#index'
   
-  get 'home/index'
-  get 'members/index', to: 'members#index', as: :user_root
+  # go to this action/page after the devise login
+  get 'members', to: 'members#index', as: :user_root
 
   devise_for :users, :skip => [:registrations]
-  
-  #resources :people
-  # add the angular 2 default style of route
-  #get '/people.json/:id', controller: 'people', action: :show, format: 'json'
-
-
-  #resources :members, only: [:show], format: 'json'
-  #match '/members.json', controller: 'members', action: :index, format: 'json', via: [:get, :options]
-  #match '/members/:id.json', controller: 'members', action: :show, format: 'json', via: [:get, :options]
-  #resources :widgets
-  
+    
   devise_scope :user do 
      get 'login', controller: 'devise/sessions', action: 'create'
      get 'logout', controller: 'devise/sessions', action: 'destroy'
@@ -30,7 +22,6 @@ Rails.application.routes.draw do
         match '/summary', controller: 'summary', action: :show, format: 'json', via: [:get, :options]
         match '/members', controller: 'members', action: :index, format: 'json', via: [:get, :options]
         match '/members/:id', controller: 'members', action: :show, format: 'json', via: [:get, :options]
-
        end
     end
   end
