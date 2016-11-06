a8695f476d494621b94f1ac0766db60de3286cfb
Pat Conley <pconley312@gmail.com>
2016-11-06 11:01:42 -0700

    order events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApiController
     puts "*** EventsController: params=#{params.inspect}"
     puts "year = #{params[:month]}"
     puts "year = #{params['month']}"
-    @events = Event.all.order(:id)
+    @events = Event.all.order(:day)
     @events = @events.for_month(params[:month].to_i) if params[:month] 
     @events = @events.for_year(params[:year].to_i) if params[:year] 
     @events = @events.for_day(params[:day].to_i) if params[:day] 
